c9f9f7d8529a0dfc4c42926b42090580c94569e0
Andrew Pearson <tparis00ap@hotmail.com>
2021-01-27 21:23:54 +0000

    Able to query import list with member names

--- src/analyze-alpha-roster.js
+++ src/analyze-alpha-roster.js
@@ -1,6 +1,21 @@
 async function analyzeAlphaRoster() {
     const record_id = await context("record_id");
-    console.debug("Test");
+    let query = `
+      {
+        allRosterimport(where: $where) {
+          results {
+            id
+            fullName
+            dutyTitle
+          }
+        }
+      }
+    `; 
+    let record = await gql(query, {
+        where: { id: {eq: record_id } },
+    });
+    let member = record.allRosterimport.results[0]
+    return "Record: " + member.fullName;
 }
 
-export { analyzeAlphaRoster };
+export default analyzeAlphaRoster;
