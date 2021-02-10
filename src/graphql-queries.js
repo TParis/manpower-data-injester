8e00c0d7460976cf400b2ca898dfc42b525f0b5b
Andrew Pearson <tparis00ap@hotmail.com>
2021-02-10 07:47:36 +0000

    After some debugging

--- src/graphql-queries.js
+++ src/graphql-queries.js
@@ -2,9 +2,61 @@ let findImportObjectWhere = `
   {
     allRosterimport(where: $where) {
       results {
-        id
-        fullName
+        acduStatus
+        afrSectionid
+        afsc2
+        afsc3
+        afsc4
+        age
+        angRollIndicator
+        assignedPas
+        assignedPasCleartext
+        attachedPas
+        availabilityCode
+        availabilityCodeCleartext
+        availabilityStatus
+        availabilityStatusCleartext
+        cafsc
+        civilianArtid
+        dafsc
+        deployAdminStatus
+        deployAdminStatusCleartext
+        deployLegalStatus
+        deployLegalStatusCleartext
+        deployPhysStatus
+        deployPhysStatusCleartext
+        deployTimeStatus
+        deployTimeStatusCleartext
+        dutyPhone
         dutyTitle
+        fullName
+        functionalCategory
+        gainingPas
+        gainingPasCleartext
+        grade
+        gradePermProj
+        homeAddress
+        homeCity
+        homePhoneNumber
+        homeState
+        homeZipCode
+        id
+        lastEvalRating
+        limitationCode
+        limitationCodeCleartext
+        maritalStatus
+        officeSymbol
+        pafsc
+        perfIndicator
+        recordStatus
+        reenlEligStatus
+        secClr
+        spouseSsan
+        ssan
+        supvName
+        techid
+        typeSecInv
+        uifCode
       }
     }
   }
@@ -14,7 +66,61 @@ let findRosterObjectWhere = `
   {
     allRoster(where: $where) {
       results {
+        acduStatus
+        afrSectionid
+        afsc2
+        afsc3
+        afsc4
+        age
+        angRollIndicator
+        assignedPas
+        assignedPasCleartext
+        attachedPas
+        availabilityCode
+        availabilityCodeCleartext
+        availabilityStatus
+        availabilityStatusCleartext
+        cafsc
+        civilianArtid
+        dafsc
+        deployAdminStatus
+        deployAdminStatusCleartext
+        deployLegalStatus
+        deployLegalStatusCleartext
+        deployPhysStatus
+        deployPhysStatusCleartext
+        deployTimeStatus
+        deployTimeStatusCleartext
+        dutyPhone
+        dutyTitle
+        fullName
+        functionalCategory
+        gainingPas
+        gainingPasCleartext
+        grade
+        gradePermProj
+        homeAddress
+        homeCity
+        homePhoneNumber
+        homeState
+        homeZipCode
         id
+        lastEvalRating
+        limitationCode
+        limitationCodeCleartext
+        maritalStatus
+        officeSymbol
+        pafsc
+        perfIndicator
+        recordStatus
+        reenlEligStatus
+        secClr
+        spouseSsan
+        ssan
+        supvName
+        techid
+        typeSecInv
+        uifCode
       }
     }
   }
